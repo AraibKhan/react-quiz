8f2bb35a1830a75ad19238092a9bcd2be5c2e695
Araib Khan <AraibKhan93@gmail.com>
2023-08-08 22:44:25 +0500

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./components/Main";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:8000/questions");
+      const data = await res.json();
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        <p>Question?</p>
+      </Main>
     </div>
   );
-}
+};
 
 export default App;
