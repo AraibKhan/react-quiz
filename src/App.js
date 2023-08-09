eb02221ef60e6d28d8153a971332cab1ae7ecbcb
Araib Khan <AraibKhan93@gmail.com>
2023-08-09 11:54:15 +0500

    Created a fake REST API to fetch data.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import { useEffect } from "react";
-import Header from "./Header";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
 import Main from "./components/Main";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Invalid action!");
+  }
+};
+const initialState = {
+  questions: [],
+
+  //loading, error, ready, active, finished
+  status: "loading",
+};
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("http://localhost:8000/questions");
-      const data = await res.json();
-      console.log(data);
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (e) {
+        dispatch({ type: "dataFailed" });
+      }
     };
 
     fetchData();
